1f8d1a608c34356e23bd97cbbcc4c4bdf8bb02b5
RIddhi-Banik <157967623+Riddhi-Banik@users.noreply.github.com>
2024-03-11 23:44:01 +0600

    Updated nid.py
    patched some bugs and made it capable of basic functionalities

--- nid.py
+++ nid.py
@@ -1,5 +1,5 @@
 nid = []
-
+# patched the error and added a few extra features
 def add_entry(entry):
     nid.append(entry)
 
@@ -16,20 +16,23 @@ def update_entry(entry, new_entry):
             nid[i] = new_entry
 
 
+while True:
+    try:
+        user_input = input("Enter 1 to add, 2 to display, 3 to delete and 4 to update: ")
 
-user_input = input["Enter 1 to add, 2 to display, 3 to delete and 4 to update"]
-
-if user_input == "1":
-    add_num = input("Enter NID num: ")
-    add_entry(add_num)
-elif user_input =="2":
-    display_entry()
-elif user_input =="3":
-    remove_num = input("Enter NID num that you want to remove: ")
-    delete_entry(remove_num)
-elif user_input=="4":
-    num_to_update = input("Enter the number you want to remove: ")
-    enter_new_num = input("Enter new number: ")
-    update_entry(num_to_update,enter_new_num)
-else:
-    print("Invalid entry!")
+        if user_input == "1":
+            add_num = input("Enter NID num: ")
+            add_entry(add_num)
+        elif user_input =="2":
+            display_entry()
+        elif user_input =="3":
+            remove_num = input("Enter NID num that you want to remove: ")
+            delete_entry(remove_num)
+        elif user_input=="4":
+            num_to_update = input("Enter the number you want to remove: ")
+            enter_new_num = input("Enter new number: ")
+            update_entry(num_to_update,enter_new_num)
+        else:
+            print("Invalid entry!")
+    except Exception as ex:
+        print("Something went wrong")
